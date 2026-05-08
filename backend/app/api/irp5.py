e394871449d83414ded85869a3d91a0a926ca354
Wjm Piek <wjm@martinsdirect.com>
2026-05-08 12:59:29 +0200

    fix missing Header import

--- backend/app/api/irp5.py
+++ backend/app/api/irp5.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from pathlib import Path
 import uuid
 
-from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Header
 from fastapi.responses import FileResponse
 from sqlalchemy import text
 from sqlalchemy.orm import Session
